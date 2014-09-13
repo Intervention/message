d83b45350ec5028471f4d7b494d389c0f45ba844
Oliver Vogel <oliver@olivervogel.net>
2014-09-13 22:20:45 +0200

    testing

--- src/Intervention/Message/Message.php
+++ src/Intervention/Message/Message.php
@@ -2,26 +2,28 @@
 
 namespace Intervention\Message;
 
+use Session, View;
+
 class Message
 {
-    public static function add($message)
+    public function add($message)
     {
         $messages = array();
         
-        if (\Session::has('messages')) {
-            $messages = \Session::get('messages');
+        if (Session::has('messages')) {
+            $messages = Session::get('messages');
         }
 
         $messages[] = $message;
 
-        \Session::flash('messages', $messages);
+        Session::flash('messages', $messages);
     }
 
-    public static function display()
+    public function display()
     {
-    	if (\Session::has('messages')) {
-    		return \View::make('message::message', array(
-    			'messages' => \Session::get('messages')
+    	if (Session::has('messages')) {
+    		return View::make('message::message', array(
+    			'messages' => Session::get('messages')
     		));
     	}
     }
